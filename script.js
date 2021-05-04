fb332a0c517dd6f684dc5ddc493de6927e8fb595
Your Name <you@example.com>
2021-05-04 14:55:01 +0300

    first update

---
+++ script.js
@@ -0,0 +1,44 @@
+// Selectors
+const btnNext = document.querySelector('.btn-next')
+const btnPrev = document.querySelector('.btn-prev')
+const btnStart = document.querySelector('.btn-start')
+const btnRandom = document.querySelector('.btn-rand')
+
+let count = 1;
+
+
+btnStart.addEventListener('click', function() {
+  document.querySelector('.containerStartPage').style.display = 'none'
+})
+
+
+btnNext.addEventListener('click', function() {
+
+  if (count < 27) {
+    count++
+    document.querySelector('.container').style.backgroundImage = `url(img/${count}.jpeg)`
+  } else {
+    count = 1
+    document.querySelector('.container').style.backgroundImage = `url(img/${count}.jpeg)`
+  }
+})
+
+
+btnPrev.addEventListener('click', function() {
+
+  if (count > 1) {
+    count--
+    document.querySelector('.container').style.backgroundImage = `url(img/${count}.jpeg)`
+  } else {
+    count = 27
+    document.querySelector('.container').style.backgroundImage = `url(img/${count}.jpeg)`
+  }
+})
+
+
+btnRandom.addEventListener('click', function() {
+
+  count = Math.trunc(Math.random() * 26) + 1;
+  
+  document.querySelector('.container').style.backgroundImage = `url(img/${count}.jpeg)`
+})

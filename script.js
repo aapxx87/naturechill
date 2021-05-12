d672cd8ec9221f7dd3c0b7cf4bbf9d71c22d0013
Your Name <you@example.com>
2021-05-12 18:52:05 +0300

    update

--- script.js
+++ script.js
@@ -11,6 +11,10 @@
   const btnRandom = document.querySelector('.btn-rand')
   const btnList = document.querySelector('.btn-list')
 
+  // экран просмотра горизонтальных изображений
+  const btnNextHoriz = document.querySelector('.btn-next-horiz')
+  const btnPrevHoriz = document.querySelector('.btn-prev-horiz')
+
 
   // --Containers--
   const containerStartPage = document.querySelector('.container-Start_Page')  // Стартовый экран
@@ -18,6 +22,7 @@
   const changePanel = document.querySelector('.buttons-Box--Main_Img_View')    // Блок кнопок на основном экране
   const containerAllPreview = document.querySelector('.container-Img_Preview') // Контейнер выезжающий с превью изображений
   const scrollBox = document.querySelector('.container-Img_Preview--scrollBox') // Скролл контейнер внутри "Контейнерf с превью изображений"
+  const horizontalImgBox = document.querySelector('.horiz-img-view-box') // контейнер в который вставляются горизонтальные изображения
 
   const overlay = document.querySelector('.overlay') // overlay на основном эксране при открытии блока превью
 //
@@ -77,6 +82,65 @@ const imgBaseArr = [
   ['img48', 'horizontal', 'https://cdn.dribbble.com/users/192276/screenshots/2500960/attachments/491748/midnightinthevalley-2560x1440.png', 'https://www.pinterest.ru/pin/714031715923172475/', 'https://dribbble.com/shots/2500960-Midnight-in-the-Valley/attachments/491748', ['night']],
   ['img49', 'horizontal', 'https://i.pinimg.com/564x/72/96/9f/72969fd2668e33d52f215a3573b485a0.jpg', 'https://www.pinterest.ru/pin/465207836506248851/', '', ['night']],
   ['img50', 'horizontal', 'https://i.pinimg.com/564x/e3/bb/d8/e3bbd89d4f1af87d9fa6607d5f2e7382.jpg', 'https://www.pinterest.ru/pin/714031715923216502/', '', ['']],
+
+  ['img51', 'vertical', 'https://i.pinimg.com/564x/7f/cb/88/7fcb886a50663fd94842c526068e987d.jpg', 'https://www.pinterest.ru/pin/411797959670539181/', '', ['']],
+
+  ['img52', 'vertical', 'https://i.pinimg.com/564x/6a/4a/de/6a4adec7a358f20b3957856dc6eddede.jpg', 'https://www.pinterest.ru/pin/202662051954345909/', 'https://www.deviantart.com/balance-sheet/art/SV3-5-Sundown-330292991', ['']],
+
+  ['img53', 'vertical', 'https://i.pinimg.com/564x/88/03/05/8803058270278d313eff2eb5e354ab57.jpg', 'https://www.pinterest.ru/pin/458311699573919927/', 'https://www.wattpad.com/405208886-daily-inspiration-day-3?utm_source=web&utm_medium=pinterest&utm_content=share_reading', ['']],
+
+  ['img54', 'vertical', 'https://i.pinimg.com/564x/37/f6/03/37f6031bf63a5cedeb394d1246270cb1.jpg', 'https://www.pinterest.ru/pin/44684221289241004/', '', ['']],
+
+  ['img55', 'vertical', 'https://i.redd.it/cfo4zqs7hur01.png', 'https://www.pinterest.ru/pin/808818414302714610/', 'https://www.reddit.com/r/Art/comments/8c6kld/hills_digital_1500x2287px/?utm_source=ifttt', ['']],
+
+  ['img56', 'vertical', 'https://i.pinimg.com/564x/da/67/11/da6711a71b6c8c17fc79bc3e6f08c351.jpg', 'https://www.pinterest.ru/pin/769200811344729381/', '', ['']],
+
+  ['img56', 'vertical', 'https://i.pinimg.com/564x/c3/de/be/c3debeb47ec21910170a8b6619e4f995.jpg', 'https://www.pinterest.ru/pin/839428818028569558/', '', ['']],
+
+  ['img57', 'vertical', 'https://i.pinimg.com/564x/8e/92/60/8e92608f30827965f99c7792598d9200.jpg', 'https://www.pinterest.ru/pin/714031715923312062/', '', ['']],
+
+  ['img58', 'vertical', 'https://i.pinimg.com/564x/76/f4/08/76f4086b08db98587576cf2304570367.jpg', 'https://www.pinterest.ru/pin/714031715923312037/', '', ['']],
+
+  ['img59', 'vertical', 'https://i.pinimg.com/564x/96/af/bf/96afbf8dd580a8fe959a8723aa233918.jpg', 'https://www.pinterest.ru/pin/714031715923237596/', '', ['']],
+
+  ['img60', 'horizontal', 'https://i.pinimg.com/564x/91/5e/4c/915e4cf8f21f1ed8cfce9f66ac4c64d4.jpg', 'https://www.pinterest.ru/pin/714031715923445339/', '', ['']],
+
+  ['img61', 'horizontal', 'https://i.pinimg.com/564x/53/da/3a/53da3ab238c3dc4f88e09bed9c2a2653.jpg', 'https://www.pinterest.ru/pin/714031715923385820/', '', ['']],
+
+  ['img62', 'horizontal', 'https://free4kwallpapers.com/uploads/originals/2019/01/29/minimal-japan-wallpaper.jpg', 'https://www.pinterest.ru/pin/714031715923385817/', '', ['']],
+
+  ['img63', 'horizontal', 'https://i.pinimg.com/564x/61/3e/53/613e53cc12574047390b3092424c19bb.jpg', 'https://www.pinterest.ru/pin/714031715923385811/', '', ['']],
+
+  ['img64', 'horizontal', 'https://i.pinimg.com/564x/ad/66/17/ad66173fbfb98ff0458cfc7cd1d4be12.jpg', 'https://www.pinterest.ru/pin/714031715923385807/', '', ['']],
+
+  ['img65', 'horizontal', 'https://i.pinimg.com/564x/7b/23/eb/7b23ebe8e8230e543d1669a864fa3996.jpg', 'https://www.pinterest.ru/pin/714031715923385779/', '', ['']],
+
+  ['img66', 'horizontal', 'https://i.pinimg.com/564x/75/82/57/75825789a3dae9d79d39313e4bde1868.jpg', 'https://www.pinterest.ru/pin/714031715923385755/', '', ['']],
+
+  ['img67', 'horizontal', 'https://i.pinimg.com/564x/b8/58/25/b85825b47e3af963481835d1c540ff64.jpg', 'https://www.pinterest.ru/pin/714031715923385774/', '', ['']],
+
+  ['img68', 'horizontal', 'https://i.pinimg.com/564x/ff/4c/a0/ff4ca09a82803447d00fc6f942ea35c5.jpg', 'https://www.pinterest.ru/pin/714031715923385741/', '', ['']],
+
+  ['img69', 'horizontal', 'https://i.pinimg.com/564x/53/18/62/5318621ee33750927f2f78a30a67816d.jpg', 'https://www.pinterest.ru/pin/714031715923384869/', '', ['']],
+
+  ['img70', 'horizontal', 'https://i.pinimg.com/564x/43/10/be/4310be2a1e733e14169ea5117d86c23a.jpg', 'https://www.pinterest.ru/pin/714031715923384814/', '', ['']],
+
+  ['img71', 'horizontal', 'https://i.pinimg.com/564x/83/b0/11/83b0117384e82632e83cc4741bdc5081.jpg', 'https://www.pinterest.ru/pin/714031715923216706/', '', ['']],
+
+  ['img72', 'horizontal', 'https://i.pinimg.com/564x/47/6b/17/476b1793984a510710f431dc68432365.jpg', 'https://www.pinterest.ru/pin/714031715923385737/', '', ['']],
+
+  ['img73', 'horizontal', 'https://i.pinimg.com/564x/84/55/bc/8455bc57cafc1049fc8eb6d40a240b0a.jpg', 'https://www.pinterest.ru/pin/714031715923276931/', '', ['']],
+
+  ['img74', 'horizontal', 'https://i.pinimg.com/564x/48/e4/6d/48e46d86f9fc73b3e9b009b6907dbc0a.jpg', 'https://www.pinterest.ru/pin/714031715923276944/', '', ['']],
+
+  ['img75', 'horizontal', 'https://i.pinimg.com/564x/86/37/96/863796feee8a8ec21578b08150975564.jpg', 'https://www.pinterest.ru/pin/621567186065334105/', '', ['']],
+
+  ['img76', 'horizontal', 'https://i.pinimg.com/564x/a9/87/08/a987088fea7fad2df0ccb1305fabcc36.jpg', 'https://www.pinterest.ru/pin/714031715923216569/', '', ['']],
+
+  ['img77', 'horizontal', 'https://i.pinimg.com/564x/8e/3f/59/8e3f59787f6802df7cf2b31b8b36d396.jpg', 'https://www.pinterest.ru/pin/508273507944052418/', '', ['']],
+
+  ['img78', 'horizontal', 'https://i.pinimg.com/564x/d1/0c/d8/d10cd853ce75f3e5bfa2cf6f42e371d1.jpg', 'https://www.pinterest.ru/pin/741194051160492223/', '', ['']],
+
 ]
 
 
@@ -93,6 +157,23 @@ const imgBaseArr = [
       imgUrlFilterArrVertical.push(imgBaseArr[i][2])
     }
   }
+//
+
+
+// Выбираем из массива элементы Horizontal
+
+  // пустой массив, который будет заполняться url вертикальных элементов при фильтрации
+  const imgUrlFilterArrHorizontal = []
+
+
+  // прогоняем циклом и фильтруем элементы с фильтром 'vertical', их url загоняем в массив imgUrlFilterArrVertical
+  for(let i = 0; i < imgBaseArr.length; i++) {
+    if(imgBaseArr[i][1] == 'horizontal') {
+      imgUrlFilterArrHorizontal.push(imgBaseArr[i][2])
+    }
+  }
+
+  const numbersHorisArray = imgUrlFilterArrHorizontal.length
 //
 
 
@@ -117,18 +198,26 @@ const imgBaseArr = [
   // перемнная к которой привязаны смены изображений
   let count = 0;
 
+  let countHorizontal = 0;
+
   // вставляем первую картинку в главное окнг просмотра изображений
   containerImg.style.backgroundImage = `url(${imgUrlFilterArrVertical[count]})`
+
+  // вставляем первую картинку в окно просмотра горизонтальных изображений
+  horizontalImgBox.src = `${imgUrlFilterArrHorizontal[countHorizontal]}`
+
+  console.log(imgUrlFilterArrHorizontal);
 //
 
 
 
 // Buttons Actions + Overlay
+
   btnStart.addEventListener('click', function() {
     containerStartPage.style.transform = 'translateY(-100vh)'
   })
 
-
+  // вертикальные изображения
   btnNext.addEventListener('click', function() {
 
     if (count < numbersVertArray - 1) {
@@ -178,6 +267,29 @@ const imgBaseArr = [
     changePanel.style.transform = 'translateY(0)'
     containerAllPreview.style.zIndex = '-30'
     containerAllPreview.style.transform = 'translateY(300px)'
+  })
+
+  // горизонтальные изображения
+  btnNextHoriz.addEventListener('click', function() {
+
+    if (countHorizontal < numbersHorisArray - 1) {
+      countHorizontal++
+      horizontalImgBox.src = imgUrlFilterArrHorizontal[countHorizontal]
+    } else {
+      countHorizontal = 0
+      horizontalImgBox.src = imgUrlFilterArrHorizontal[countHorizontal]
+    }
+  })
+
+  btnPrevHoriz.addEventListener('click', function() {
+
+    if (countHorizontal >= 1) {
+      countHorizontal--
+      horizontalImgBox.src = imgUrlFilterArrHorizontal[countHorizontal]
+    } else {
+      countHorizontal = numbersHorisArray - 1
+      horizontalImgBox.src = imgUrlFilterArrHorizontal[countHorizontal]
+    }
   })
 //
 
@@ -233,6 +345,14 @@ document.querySelector('.section-Main_Page-Vertical-button').addEventListener('c
 
   document.querySelector('.section-Main_Page').style.display = 'none'
   document.querySelector('.section-Vertical_Img').style.display = 'block'
+})
+
+
+// при клике на заголовок Horizontal главная страница скрывается, а просмотр изображений появляется
+document.querySelector('.section-Main_Page-Horizontal-button').addEventListener('click', function() {
+
+  document.querySelector('.section-Main_Page').style.display = 'none'
+  document.querySelector('.section-Horizontal_Img').style.display = 'block'
 })
 
 

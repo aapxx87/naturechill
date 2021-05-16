9ed73495652409f17ee32788e97d5c6e2bf11471
Your Name <you@example.com>
2021-05-16 21:25:14 +0300

    update

--- script.js
+++ script.js
@@ -82,65 +82,59 @@ const imgBaseArr = [
   ['img48', 'horizontal', 'https://cdn.dribbble.com/users/192276/screenshots/2500960/attachments/491748/midnightinthevalley-2560x1440.png', 'https://www.pinterest.ru/pin/714031715923172475/', 'https://dribbble.com/shots/2500960-Midnight-in-the-Valley/attachments/491748', ['night']],
   ['img49', 'horizontal', 'https://i.pinimg.com/564x/72/96/9f/72969fd2668e33d52f215a3573b485a0.jpg', 'https://www.pinterest.ru/pin/465207836506248851/', '', ['night']],
   ['img50', 'horizontal', 'https://i.pinimg.com/564x/e3/bb/d8/e3bbd89d4f1af87d9fa6607d5f2e7382.jpg', 'https://www.pinterest.ru/pin/714031715923216502/', '', ['']],
-
   ['img51', 'vertical', 'https://i.pinimg.com/564x/7f/cb/88/7fcb886a50663fd94842c526068e987d.jpg', 'https://www.pinterest.ru/pin/411797959670539181/', '', ['']],
-
   ['img52', 'vertical', 'https://i.pinimg.com/564x/6a/4a/de/6a4adec7a358f20b3957856dc6eddede.jpg', 'https://www.pinterest.ru/pin/202662051954345909/', 'https://www.deviantart.com/balance-sheet/art/SV3-5-Sundown-330292991', ['']],
-
   ['img53', 'vertical', 'https://i.pinimg.com/564x/88/03/05/8803058270278d313eff2eb5e354ab57.jpg', 'https://www.pinterest.ru/pin/458311699573919927/', 'https://www.wattpad.com/405208886-daily-inspiration-day-3?utm_source=web&utm_medium=pinterest&utm_content=share_reading', ['']],
-
   ['img54', 'vertical', 'https://i.pinimg.com/564x/37/f6/03/37f6031bf63a5cedeb394d1246270cb1.jpg', 'https://www.pinterest.ru/pin/44684221289241004/', '', ['']],
-
   ['img55', 'vertical', 'https://i.redd.it/cfo4zqs7hur01.png', 'https://www.pinterest.ru/pin/808818414302714610/', 'https://www.reddit.com/r/Art/comments/8c6kld/hills_digital_1500x2287px/?utm_source=ifttt', ['']],
-
   ['img56', 'vertical', 'https://i.pinimg.com/564x/da/67/11/da6711a71b6c8c17fc79bc3e6f08c351.jpg', 'https://www.pinterest.ru/pin/769200811344729381/', '', ['']],
-
   ['img56', 'vertical', 'https://i.pinimg.com/564x/c3/de/be/c3debeb47ec21910170a8b6619e4f995.jpg', 'https://www.pinterest.ru/pin/839428818028569558/', '', ['']],
-
   ['img57', 'vertical', 'https://i.pinimg.com/564x/8e/92/60/8e92608f30827965f99c7792598d9200.jpg', 'https://www.pinterest.ru/pin/714031715923312062/', '', ['']],
-
   ['img58', 'vertical', 'https://i.pinimg.com/564x/76/f4/08/76f4086b08db98587576cf2304570367.jpg', 'https://www.pinterest.ru/pin/714031715923312037/', '', ['']],
-
   ['img59', 'vertical', 'https://i.pinimg.com/564x/96/af/bf/96afbf8dd580a8fe959a8723aa233918.jpg', 'https://www.pinterest.ru/pin/714031715923237596/', '', ['']],
-
   ['img60', 'horizontal', 'https://i.pinimg.com/564x/91/5e/4c/915e4cf8f21f1ed8cfce9f66ac4c64d4.jpg', 'https://www.pinterest.ru/pin/714031715923445339/', '', ['']],
-
   ['img61', 'horizontal', 'https://i.pinimg.com/564x/53/da/3a/53da3ab238c3dc4f88e09bed9c2a2653.jpg', 'https://www.pinterest.ru/pin/714031715923385820/', '', ['']],
-
   ['img62', 'horizontal', 'https://free4kwallpapers.com/uploads/originals/2019/01/29/minimal-japan-wallpaper.jpg', 'https://www.pinterest.ru/pin/714031715923385817/', '', ['']],
-
   ['img63', 'horizontal', 'https://i.pinimg.com/564x/61/3e/53/613e53cc12574047390b3092424c19bb.jpg', 'https://www.pinterest.ru/pin/714031715923385811/', '', ['']],
-
   ['img64', 'horizontal', 'https://i.pinimg.com/564x/ad/66/17/ad66173fbfb98ff0458cfc7cd1d4be12.jpg', 'https://www.pinterest.ru/pin/714031715923385807/', '', ['']],
-
   ['img65', 'horizontal', 'https://i.pinimg.com/564x/7b/23/eb/7b23ebe8e8230e543d1669a864fa3996.jpg', 'https://www.pinterest.ru/pin/714031715923385779/', '', ['']],
-
   ['img66', 'horizontal', 'https://i.pinimg.com/564x/75/82/57/75825789a3dae9d79d39313e4bde1868.jpg', 'https://www.pinterest.ru/pin/714031715923385755/', '', ['']],
-
   ['img67', 'horizontal', 'https://i.pinimg.com/564x/b8/58/25/b85825b47e3af963481835d1c540ff64.jpg', 'https://www.pinterest.ru/pin/714031715923385774/', '', ['']],
-
   ['img68', 'horizontal', 'https://i.pinimg.com/564x/ff/4c/a0/ff4ca09a82803447d00fc6f942ea35c5.jpg', 'https://www.pinterest.ru/pin/714031715923385741/', '', ['']],
-
   ['img69', 'horizontal', 'https://i.pinimg.com/564x/53/18/62/5318621ee33750927f2f78a30a67816d.jpg', 'https://www.pinterest.ru/pin/714031715923384869/', '', ['']],
-
   ['img70', 'horizontal', 'https://i.pinimg.com/564x/43/10/be/4310be2a1e733e14169ea5117d86c23a.jpg', 'https://www.pinterest.ru/pin/714031715923384814/', '', ['']],
-
   ['img71', 'horizontal', 'https://i.pinimg.com/564x/83/b0/11/83b0117384e82632e83cc4741bdc5081.jpg', 'https://www.pinterest.ru/pin/714031715923216706/', '', ['']],
-
   ['img72', 'horizontal', 'https://i.pinimg.com/564x/47/6b/17/476b1793984a510710f431dc68432365.jpg', 'https://www.pinterest.ru/pin/714031715923385737/', '', ['']],
-
   ['img73', 'horizontal', 'https://i.pinimg.com/564x/84/55/bc/8455bc57cafc1049fc8eb6d40a240b0a.jpg', 'https://www.pinterest.ru/pin/714031715923276931/', '', ['']],
-
   ['img74', 'horizontal', 'https://i.pinimg.com/564x/48/e4/6d/48e46d86f9fc73b3e9b009b6907dbc0a.jpg', 'https://www.pinterest.ru/pin/714031715923276944/', '', ['']],
-
   ['img75', 'horizontal', 'https://i.pinimg.com/564x/86/37/96/863796feee8a8ec21578b08150975564.jpg', 'https://www.pinterest.ru/pin/621567186065334105/', '', ['']],
-
   ['img76', 'horizontal', 'https://i.pinimg.com/564x/a9/87/08/a987088fea7fad2df0ccb1305fabcc36.jpg', 'https://www.pinterest.ru/pin/714031715923216569/', '', ['']],
-
   ['img77', 'horizontal', 'https://i.pinimg.com/564x/8e/3f/59/8e3f59787f6802df7cf2b31b8b36d396.jpg', 'https://www.pinterest.ru/pin/508273507944052418/', '', ['']],
-
   ['img78', 'horizontal', 'https://i.pinimg.com/564x/d1/0c/d8/d10cd853ce75f3e5bfa2cf6f42e371d1.jpg', 'https://www.pinterest.ru/pin/741194051160492223/', '', ['']],
 
+  ['img79', 'horizontal', 'https://i.pinimg.com/564x/3c/d4/86/3cd4868acd05152efe05ec9a3bad352e.jpg', 'https://www.pinterest.ru/pin/714031715923582070/', '', ['']],
+  ['img80', 'horizontal', 'https://i.pinimg.com/564x/a8/b0/ee/a8b0ee5a89e093ef6448f198c01fe8df.jpg', 'https://www.pinterest.ru/pin/714031715923582063/', '', ['']],
+  ['img81', 'horizontal', 'https://i.pinimg.com/564x/3d/25/34/3d253440c68ea63b86806912e278746f.jpg', 'https://www.pinterest.ru/pin/714031715923581997/', '', ['']],
+  ['img81', 'horizontal', 'https://i.pinimg.com/564x/e1/b7/3f/e1b73f065a3e78eccca3f25000081c2a.jpg', 'https://www.pinterest.ru/pin/714031715923581980/', '', ['']],
+  ['img82', 'horizontal', 'https://i.pinimg.com/564x/33/f9/7b/33f97bb42c47a70d99c05891733bc38f.jpg', 'https://www.pinterest.ru/pin/714031715923581975/', '', ['']],
+  ['img83', 'horizontal', 'https://i.pinimg.com/564x/3f/49/74/3f4974bcf8b4b2a71663b825c2aea282.jpg', 'https://www.pinterest.ru/pin/714031715923581176/', '', ['']],
+  ['img84', 'horizontal', 'https://i.pinimg.com/564x/b7/f8/77/b7f877487511a58a3befed50f5bb46d2.jpg', 'https://www.pinterest.ru/pin/714031715923581145/', '', ['']],
+  ['img85', 'horizontal', 'https://i.pinimg.com/564x/69/51/76/69517661b817c066d58569e5e21aaf1a.jpg', 'https://www.pinterest.ru/pin/714031715923581876/', '', ['']],
+  ['img86', 'horizontal', 'https://i.pinimg.com/564x/fa/9b/40/fa9b4044348846f50fa1e10a5d80a8b5.jpg', 'https://www.pinterest.ru/pin/714031715923581135/', '', ['']],
+  ['img87', 'horizontal', 'https://i.pinimg.com/564x/2a/c6/aa/2ac6aadd584460fe6c713ca7faefedab.jpg', 'https://www.pinterest.ru/pin/714031715923581158/', '', ['']],
+  ['img88', 'horizontal', 'https://i.pinimg.com/564x/f5/38/70/f538704e9c88dcecd8bd59450c6f6166.jpg', 'https://www.pinterest.ru/pin/714031715923581113/', '', ['']],
+  ['img89', 'horizontal', 'https://i.pinimg.com/564x/1f/82/ee/1f82eeadee88c0acbc8c5364538824ed.jpg', 'https://www.pinterest.ru/pin/714031715923581130/', '', ['']],
+  ['img90', 'horizontal', 'https://i.pinimg.com/564x/94/3b/53/943b5303f8b5c15e21b28add5aee1600.jpg', 'https://www.pinterest.ru/pin/714031715923581110/', '', ['']],
+  ['img91', 'horizontal', 'https://i.pinimg.com/564x/d3/ee/b5/d3eeb5a2d36ad30746f8f348a276b09b.jpg', 'https://www.pinterest.ru/pin/714031715923581097/', '', ['']],
+  ['img92', 'horizontal', 'https://i.pinimg.com/564x/34/0c/23/340c23d6c950c2253c0a6662a2844388.jpg', 'https://www.pinterest.ru/pin/714031715923581081/', '', ['']],
+  ['img93', 'horizontal', 'https://i.pinimg.com/564x/28/68/4c/28684cb0f94883908edaf01beb785400.jpg', 'https://www.pinterest.ru/pin/714031715923581058/', '', ['']],
+  ['img94', 'horizontal', 'https://i.pinimg.com/564x/a3/88/8c/a3888c1e0f2f2cc377140baedbdda912.jpg', 'https://www.pinterest.ru/pin/714031715923581036/', '', ['']],
+  ['img95', 'horizontal', 'https://i.pinimg.com/originals/0f/72/1c/0f721c9caa7f8de789f25b68159a8297.gif', 'https://www.pinterest.ru/pin/714031715923581229/', '', ['']],
+  ['img96', 'horizontal', 'https://i.pinimg.com/originals/d7/82/d2/d782d2d6a0340307a8b39e075f7023c0.gif', 'https://www.pinterest.ru/pin/714031715923581318/', '', ['']],
+  ['img97', 'horizontal', 'https://i.pinimg.com/originals/17/c9/e0/17c9e02083ff78627364b46f71b9f3bd.gif', 'https://www.pinterest.ru/pin/714031715923581340/', '', ['']],
+  ['img98', 'horizontal', 'https://i.pinimg.com/originals/5c/e7/cc/5ce7cc39de06ef5a1be3f6f53684ac85.gif', 'https://www.pinterest.ru/pin/714031715923581212/', '', ['']],
+  ['img99', 'horizontal', 'https://i.pinimg.com/originals/03/43/f9/0343f96e901833f83bb676c796be9e32.gif', 'https://www.pinterest.ru/pin/714031715923581334/', '', ['']],
+  ['img100', 'horizontal', 'https://i.pinimg.com/originals/88/25/7f/88257fc06f6e674a8ffc2a39bd3de33a.gif', 'https://www.pinterest.ru/pin/714031715923581259/', '', ['']],
 ]
 
 
@@ -360,6 +354,14 @@ document.querySelector('.section-Main_Page-Horizontal-button').addEventListener(
 
 // при клике на кнопку menu на экране просомтра изображений при переходе на главный экране обновляются изображения в блоке vertical
 document.querySelector('.btn-menu').addEventListener('click', function() {
+
+  imgVerticalPrevieDownload()
+
+  document.querySelector('.section-Main_Page').style.display = 'block'
+  document.querySelector('.section-Vertical_Img').style.display = 'none'
+})
+
+document.querySelector('.btn-menu-horiz').addEventListener('click', function() {
 
   imgVerticalPrevieDownload()
 
